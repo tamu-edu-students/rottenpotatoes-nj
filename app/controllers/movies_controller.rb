a7aeb7ac6761f4b83748776bb1d7b0391bd92848
Neeraj Julian Joseph Rajkumar <neerajjulian@gmail.com>
2024-09-13 20:03:38 -0500

    Sorting order has been retained across pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -44,9 +44,10 @@ class MoviesController < ApplicationController
 
   # PATCH/PUT /movies/1 or /movies/1.json
   def update
+    @movie = Movie.find(params[:id])
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to movie_url(@movie), notice: "Movie was successfully updated." }
+        format.html { redirect_to movies_path(attribute: params[:attribute], order: params[:order]), notice: "Movie was successfully updated." }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -57,11 +58,9 @@ class MoviesController < ApplicationController
 
   # DELETE /movies/1 or /movies/1.json
   def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy!
-
+    @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_path(attribute: @attribute, order: @order), notice: "Movie was successfully destroyed." }
+      format.html { redirect_to movies_path(attribute: params[:attribute], order: params[:order]), notice: "Movie was successfully destroyed." }
       format.json { head :no_content }
     end
   end
